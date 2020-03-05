81c93eab59e72155e50264e3df15e035c4cfc526
jgondellier <jgondellier@yahoo.fr>
2020-03-05 22:49:35 +0100

    init

--- src/Controller/UniversignController.php
+++ src/Controller/UniversignController.php
@@ -2,7 +2,16 @@
 
 namespace App\Controller;
 
+use App\Universign\Utils\MatchAccount;
+use GuzzleHttp\Client;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -15,14 +24,15 @@ class UniversignController extends AbstractController
      */
     public function index(Request $request)
     {
-        $defaultData = ['message' => 'Type your message here'];
+        $defaultData = ['name' => ''];
         $form = $this->createFormBuilder($defaultData)
-            ->add('name', TextType::class)
+            ->add('lastname', TextType::class)
             ->add('firstname', TextType::class)
-            ->add('birthdate', TextType::class)
+            ->add('birthdate', BirthdayType::class,['required' => false,])
             ->add('email', EmailType::class)
-            ->add('tel', TextType::class)
-            ->add('message', TextareaType::class)
+            ->add('mobile', TelType::class)
+            ->add('cni1', FileType::class,['required' => false,])
+            ->add('cni2', FileType::class,['required' => false,])
             ->add('send', SubmitType::class)
             ->getForm();
 
@@ -31,10 +41,31 @@ class UniversignController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             // data is an array with "name", "email", and "message" keys
             $data = $form->getData();
+            //var_dump($data);
+            $uri = 'https://'.$this->getParameter('univ.user').':'.$this->getParameter('univ.pass').'@'.$this->getParameter('univ.path');
+            $params = array('firstname'=>$data['firstname'],'lastname'=>$data['lastname'],'email'=>$data['email'],'mobile'=>$data['mobile']);
+            //var_dump(xmlrpc_encode($params) );
+            $client = new Client([
+                'base_uri'  => '',
+                'timeout'   => 200.0,
+                'verify'    => false,
+            ]);
+
+            $response = $client->request('GET', $uri, ['body' => xmlrpc_encode_request('matcher.matchAccount',$params)]);
+            $t_response = xmlrpc_decode($response->getBody()->getContents());
+            $matchAccountresult = new MatchAccount($t_response);
+
+            return $this->render('universign/index.html.twig', [
+                'form' => $form->createView(),
+                'bestResult' => $matchAccountresult->getBestResult(),
+                'explanation' => $matchAccountresult->getExplanation(),
+                'isValid1' => $matchAccountresult->isValid(1),
+                'isValid2' => $matchAccountresult->isValid(2),
+            ]);
         }
 
         return $this->render('universign/index.html.twig', [
-            'controller_name' => 'UniversignController',
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Universign/Utils/MatchAccount.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace App\Universign\Utils;
+
+class MatchAccount
+{
+    private const OBFS='**';
+    private $xmlrpcResult;
+    private $bestResult=false;
+    private $account=false;
+    private $partialAccount=False;
+    private $isCertified=False;
+    private $explanation;
+
+    /**
+     * MatchAccount constructor.
+     * @param array $xmlrpcResult
+     */
+    public function __construct(array $xmlrpcResult)
+    {
+        $this->xmlrpcResult = $xmlrpcResult;
+        $this->findBestResult();
+        $this->explain();
+    }
+
+    /**
+     *  Donne le compte trouvé s'il y a unseul résultat et que ca correspond
+     *
+     * @return bool|array
+     */
+    private function findAccount()
+    {
+        if(empty($this->xmlrpcResult) && (count($this->xmlrpcResult) === 1) && !in_array(self::OBFS, $this->xmlrpcResult, true)) {
+            $this->isCertified=$this->isCertified($this->xmlrpcResult[0]);
+            return  $this->account = $this->xmlrpcResult[0];
+        }
+        return False;
+    }
+
+    private function findBestResult(): bool
+    {
+        if(!$this->findAccount()){
+            $bestResult = array();
+            if(empty($this->xmlrpcResult)){
+                return False;
+            }
+
+            foreach($this->xmlrpcResult as $result){
+                //On a trouvé l'email
+                if(strpos($result['email'],self::OBFS)===False){
+                    $bestResult['findEmail']=$result;
+                    if(array_key_exists('findMobile',$bestResult)){
+                        break;
+                    }
+                }
+                //On a trouvé le mobile
+                if(strpos($result['mobile'],self::OBFS)===False){
+                    $bestResult['findMobile']=$result;
+                    if(array_key_exists('findEmail',$bestResult)){
+                        break;
+                    }
+                }
+            }
+            //On a trouvé le mail et le mobile dans le meme resultat on a un compte mais PB de prenom et nom
+            if(array_key_exists('findEmail',$bestResult) && array_key_exists('findMobile',$bestResult)){
+                if($bestResult['findEmail']===$bestResult['findMobile']){
+                    $this->isCertified=$this->isCertified($bestResult);
+                    $this->partialAccount=$bestResult['findEmail'];
+                    return False;
+                }
+            }elseif(array_key_exists('findEmail',$bestResult)){
+                $this->bestResult = $bestResult;
+            }elseif(array_key_exists('findMobile',$bestResult)){
+                $this->bestResult = $bestResult;
+            }
+        }
+
+        return False;
+    }
+
+    /**
+     * Retourne le résultat le plus pertinent.
+     *
+     * @return array|bool
+     */
+    public function getBestResult()
+    {
+        if($this->account){
+            return $this->account;
+        }
+        if($this->partialAccount){
+            return $this->partialAccount;
+        }
+        return $this->bestResult;
+    }
+
+    private function isCertified(array $account): bool
+    {
+        return array_key_exists('certificateLevel', $account) && $account['certificateLevel'] === 'certified';
+    }
+
+    /**
+     * @param int $neededSignLevel Niveau de la signature souhaité
+     * @return bool
+     */
+    public function isValid($neededSignLevel): bool
+    {
+        switch ($neededSignLevel) {
+            case 1:
+                //Compte trouvé ou compte trouvé sans prenom et nom mais sans certificat
+                if(($this->partialAccount && $this->isCertified===False) || $this->account ){
+                    return true;
+                }
+                break;
+            case 2:
+                //Compte trouvé et avec certificat
+                if($this->account && $this->isCertified){
+                    return true;
+                }
+                break;
+        }
+        return False;
+    }
+
+    /**
+     * Trouve une expliquation au résultat trouvé.
+     */
+    public function explain(): void
+    {
+        if($this->account && $this->isCertified){
+            $this->explanation = 'Aucun problème pour signer n\'importe quelle acte.';
+            return;
+        }
+        if($this->account && !$this->isCertified){
+            $this->explanation = 'Attention compte de niveau 1. La CNI pourra être demandée.';
+            return;
+        }
+        if($this->partialAccount && $this->isCertified){
+            $explain = 'Compte de niveau 2 mais ';
+            if(strpos(self::OBFS,$this->partialAccount['lastname'])){
+                $explain .= 'le nom ne correspond pas au client ';
+            }
+            if(strpos(self::OBFS,$this->partialAccount['firstname'])){
+                $explain .= 'le prénom ne correspond pas au client ';
+            }
+            $explain .= '. Impossible de signer en niveau 2.';
+            $this->explanation = $explain;
+            return;
+        }
+        if($this->partialAccount && !$this->isCertified){
+            $this->explanation = 'Compte de niveau 1. Le nom ou le prenom ne correspond pas. Pas de problème pour signer en niveau 1.';
+            return;
+        }
+        if(is_array($this->bestResult)){
+            if(array_key_exists('findEmail',$this->bestResult) && array_key_exists('findMobile',$this->bestResult)){
+                $this->explanation = 'Deux comptes trouvés. Un avec l\'email et un autre avec le téléphone. L\'association email et mobile doit être unique.';
+                return;
+            }
+            if(array_key_exists('findEmail',$this->bestResult)){
+                $this->explanation = 'L\'email demandé a été trouvé mais pas le téléphone.';
+                return;
+            }
+            if(array_key_exists('findMobile',$this->bestResult)){
+                $this->explanation = 'Le mobile demandé a été trouvé mais pas l\'email.';
+                return;
+            }
+        }
+
+        if($this->account===False && $this->partialAccount===False && $this->bestResult===False){
+            $this->explanation = 'Aucun compte trouvé.';
+            return;
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getExplanation()
+    {
+        return $this->explanation;
+    }
+
+}
