a88ecd77d8096536591a0f2dfb47b8fb1b989970
jgondellier <jgondellier@yahoo.fr>
2021-02-11 18:34:12 +0100

    symfony 5.0.* to 5.1.11

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Gondellier\UniversignBundle\UniversignBundle::class =>['all' => true],
+    Gondellier\UniversignBundle\UniversignBundle::class => ['all' => true],
 ];
